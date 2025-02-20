d9b31e1bafcaed92974e12b425ae7d6d2541bf9b
abdo712005 <amm280206@gmail.com>
2025-02-20 20:47:43 +0200

    add notes view components and implement search icon

---
+++ lib/components/Notes_view_body.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:note_app/components/Custom_search_icon.dart';
+
+class NotesViewbody extends StatelessWidget {
+  const NotesViewbody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0,vertical: 8),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 30),
+            child: Row(
+              children: [
+                Text(
+                  "Notes",
+                  style: TextStyle(
+                    fontSize: 24,
+                  ),
+                ),
+                Spacer(),
+                CustomSearchIcon(),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/components/Notes_view_body.dart';
 
-class NotesView extends StatelessWidget{
-  static String id="Notesview";
+class NotesView extends StatelessWidget {
+  static String id = "Notesview";
   @override
   Widget build(BuildContext context) {
-   return Scaffold();
+    return Scaffold(
+      body: NotesViewbody(),
+    );
   }
-
 }
+
