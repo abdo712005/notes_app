1cf3defea4483dd9d9c861d58ae108b266b7d2b0
abdo712005 <amm280206@gmail.com>
2025-02-21 20:31:55 +0200

    add floating action button to NotesView for adding new notes

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/components/Add_note_button_sheet.dart';
 import 'package:note_app/components/Notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
@@ -6,8 +7,17 @@ class NotesView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(context: context, builder: (context){
+            return AddNoteButtonSheet();
+          },);
+        },
+        child: Icon(
+          Icons.add,
+        ),
+      ),
       body: NotesViewbody(),
     );
   }
 }
-
