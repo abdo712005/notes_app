cb4a430474309ed944d04962707d3e7b655f38c3
abdo712005 <amm280206@gmail.com>
2025-02-28 23:37:12 +0200

    initialize Hive for note storage and add constants for primary color and notes box

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 // ignore_for_file: use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:note_app/constants.dart';
 import 'package:note_app/views/Edit_Note_view.dart';
 import 'package:note_app/views/notes_view.dart';
 
-void main() {
+void main() async{
+  await Hive.initFlutter();
+  Hive.openBox(kNotesBox);
   runApp( Notesapp());
 }
 
