db88d345f1961bc3a06ee39e940240d154f53283
abdo712005 <amm280206@gmail.com>
2025-03-02 17:05:32 +0200

    implement addnote method in AddNoteCubit for note storage with error handling

--- lib/cubits/Add_note_cubit/add_note_cubit.dart
+++ lib/cubits/Add_note_cubit/add_note_cubit.dart
@@ -1,14 +1,27 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:meta/meta.dart';
+import 'package:note_app/constants.dart';
+import 'package:note_app/models/note_model.dart';
 
 part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
-  AddNoteCubit() : super(AddNoteInitial());{
+  AddNoteCubit() : super(AddNoteInitial());
 
-  addnote(){
-    
+  addnote( NoteModel note) async{
+    emit(AddNoteloding());
+
+   try {
+  var notesbox= Hive.box(kNotesBox);
+  await notesbox.add(note);
+  emit(AddNoteSuccess());
+}  catch (e) {
+  AddNoteFaliure(errormessage: e.toString());
+}
+
+
 
   }
-}
+
 }
